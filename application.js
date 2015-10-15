8beacf1f779d68c2ab0d083e62f2267dc4dc61b7
atsundwn <atsundwn@gmail.com>
2015-10-15 10:36:12 -0400

    Added a 'New Game' button. Added a feature to recognize draw if there's no winner and the board is full.

--- application.js
+++ application.js
@@ -1,57 +1,55 @@
 $(document).ready(function() {
 
-  var board = [null, null, null,
-               null, null, null,
-               null, null, null];
-  var player = 'O';
+  var board, player;
 
-  $('.box').on('click', function () {
+  var newGame = function() {
+    board = [null, null, null,
+             null, null, null,
+             null, null, null];
+    player = 'O';
 
+    $('#msgBox').text('O\'s Turn to Start');
+    $('.box').text('');
+    $('.box').bind('click', clickHandler);
+  }
+
+  var clickHandler = function() {
     board[$(this).attr('value')] = player;
     console.log(board);
 
     $(this).text(player);
-    $(this).off('click');
+    $(this).unbind('click', clickHandler);
 
     if((board[0] === player && board[1] === player && board[2] === player) ||
-      (board[3] === player && board[4] === player && board[5] === player) ||
-      (board[6] === player && board[7] === player && board[8] === player) ||
-      (board[0] === player && board[3] === player && board[6] === player) ||
-      (board[1] === player && board[4] === player && board[7] === player) ||
-      (board[2] === player && board[5] === player && board[8] === player) ||
-      (board[0] === player && board[4] === player && board[8] === player) ||
-      (board[2] === player && board[4] === player && board[6] === player)) {
+       (board[3] === player && board[4] === player && board[5] === player) ||
+       (board[6] === player && board[7] === player && board[8] === player) ||
+       (board[0] === player && board[3] === player && board[6] === player) ||
+       (board[1] === player && board[4] === player && board[7] === player) ||
+       (board[2] === player && board[5] === player && board[8] === player) ||
+       (board[0] === player && board[4] === player && board[8] === player) ||
+       (board[2] === player && board[4] === player && board[6] === player)) {
       alert(player + ' Wins!');
+      $('.box').unbind('click', clickHandler);
+      $('#msgBox').text('Press New Game to Play Again');
+      return;
+    } else if (board.every(elem => elem !== null)) {
+      alert('It\'s a Draw!');
+      $('.box').unbind('click', clickHandler);
+      $('#msgBox').text('Press New Game to Play Again');
+      return;
     }
-
 
     if(player === 'O') {
       player = 'X';
     } else {
         player = 'O';
       }
+
     $('#msgBox').text(player + '\'s Turn');
 
+  }
 
-
-  });
-
+  $('button').on('click', newGame);
 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-var init = function() {
-
-};
-
-init();
